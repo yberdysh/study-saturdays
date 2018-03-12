c36ee08aa9c41317f8573a5364caf4d250944afe
Dan <daniel.sohval@gmail.com>
2018-03-12 11:00:41 -0400

    added week 2 starting point

---
+++ w2/routes/student.js
@@ -0,0 +1,4 @@
+const router = require('express').Router();
+const Student = require('../db/models/students');
+
+module.exports = router;
---
+++ w2/routes/test.js
@@ -0,0 +1,5 @@
+const router = require('express').Router();
+const Test = require('../db/models/tests');
+const Student = require('../db/models/students');
+
+module.exports = router;
